b65b0cac57c1801131a14924b5684ef163d1f16f
whosefriendA <2653344989@qq.com>
2024-03-17 21:34:02 +0800

    new

--- my_shell.c
+++ my_shell.c
@@ -5,7 +5,7 @@
 #include<sys/stat.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<fcntl.>
+#include<fcntl.h>
 #include<dirent.h>
 #include<readline/readline.h>
 #include<errno.h>
@@ -13,20 +13,26 @@
 #define BLUE "\033[34m"
 #define CLOSE "\033[0m"
 void analyze_cmd(char*);
-void do_cmd(int,char**)
-int main(){
-    singal(SIGHUP,SIG_IGN);
-    singal(SIGTIN,SIG_IGN);
-    singal(SIGTOU,SIG_IGN);
-    singal(SIGINT,SIG_IGN);
-    singal(SIGSTP,SIG_IGN);
+void do_cmd(int,char**);
+int main(int argc,char**argv){
+    signal(SIGHUP,SIG_IGN);
+    signal(SIGTTIN,SIG_IGN);
+    signal(SIGTTOU,SIG_IGN);
+    signal(SIGINT,SIG_IGN);
+    signal(SIGTSTP,SIG_IGN);
     while(1){
         char pathname[PATHMAX];
-        getcwd(pathname,PATHMAX)
+        getcwd(pathname,PATHMAX);
         char*command=readline(BLUE"Whosefrienda-shell"CLOSE);//readline函数输出给出的字符串并读取一行输入，并为读取的输入动态分配内存，返回值为指向读取输入的指针
-        if(!command)perror("readline malloc failure");
+        if(!command) perror("readline malloc failure");
         analyze_cmd(command);
         do_cmd(argc,argv);
         free(command);
     }
 }
+void analyze_cmd(char*command){
+     
+};
+void do_cmd(int argc,char**argv){
+
+};
