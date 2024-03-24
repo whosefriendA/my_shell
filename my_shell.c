3ae545f02978511ef5be097564e408c7c4fe2efa
whosefriendA <2653344989@qq.com>
2024-03-24 21:13:37 +0800

    new]

--- my_shell.c
+++ my_shell.c
@@ -21,7 +21,7 @@ int i_redir=0;
 int o_redir=0;
 int _pipe=0;
 int a_o_redir=0;
-int backpro=0;//命令解析的参数
+int pass=0;//命令解析的参数
 
 void printname(void);
 int analyze_cmd(int,char**);
@@ -43,7 +43,7 @@ int main(int argc,char*argv){
   while(1){
     char*argv[MAX]={NULL};
     printname();
-    char*command=readline("");//readline函数输出给出的字符串并读取一行输入，并为读取的输入动态分配内存，返回值为指向读取输入的指针
+    char*command=readline(" ");//readline函数输出给出的字符串并读取一行输入，并为读取的输入动态分配内存，返回值为指向读取输入的指针
     if (command == NULL) continue;//屏蔽ctrl+d 
     int argc=1;  
     argv[0] = strtok(command, " ");
@@ -60,7 +60,7 @@ void printname(){
     getcwd(pathname,PATHMAX);
     printf(BLUE"Whosefrienda-shell"CLOSE);
     printf(GREEN" :%s"CLOSE,pathname);
-    printf(" $ ");
+    printf("$ ");
     fflush(stdout);
 }
 
@@ -73,26 +73,26 @@ int analyze_cmd(int argc,char*argv[]){
       if (strcmp(argv[i], ">>") == 0) a_o_redir = 1;
       if (strcmp(argv[i], "<") == 0) i_redir = 1;
       if (strcmp(argv[i], "&") == 0){
-        backpro = 1;
+        pass = 1;
         argv[i]=NULL;
       }
     }
 }
 
 void do_cmd(int argc,char*argv[]){
-  if(backpro==1) argc--;
+  if(pass==1) argc--;
   if (cd == 1) mycd(argv);
-  else if (strcmp(argv[0], "history") == 0) ShowHistory();//展示历史命令
+  else if (strcmp(argv[0], "history") == 0) showhistory();//展示历史命令
   else if (strcmp(argv[0], "exit") == 0)
   {
     printf("exit\n");
     printf("有停止的任务\n");
     exit(0);
   }
+  else if ( i_redir== 1) iredir(argv);// < 
   else if ( o_redir== 1) oredir(argv);// >
-  else if ( _pipe == 1) mymulpipe(argv, argc);// |
   else if ( a_o_redir== 1) aoredir(argv);// >>
-  else if ( i_redir== 1) iredir(argv);// <
+  else if ( _pipe == 1) mymulpipe(argv, argc);// |  管道放在最后判定，因为重定向中也有管道的判定
   else //需要fork子进程进行执行的命令
   {
     if (strcmp(argv[0], "ls") == 0)
@@ -111,7 +111,7 @@ void do_cmd(int argc,char*argv[]){
     }
     else if (pid > 0) //父进程
     {
-      if(backpro==1)
+      if(pass==1)
       {
         printf("%d\n",pid);
         return;
@@ -120,6 +120,7 @@ void do_cmd(int argc,char*argv[]){
     }
   }
 }
+
 void showhistory()
 {
   int i = 0;
@@ -128,6 +129,7 @@ void showhistory()
   while (his[i] != NULL)
     printf("%-3d   %s\n", i, his[i++]->line);
 }
+
 char lastpath[MAX];//为实现cd-而声明
 void mycd(char *argv[]){
 if (argv[1] == NULL)//未输入要跳转的目录的情况
@@ -154,6 +156,7 @@ if (argv[1] == NULL)//未输入要跳转的目录的情况
     chdir(argv[1]);
   }
 }
+
 void oredir(char *argv[]){
 char *preargv[MAX] = {NULL};
   int i = 0;
@@ -162,7 +165,7 @@ char *preargv[MAX] = {NULL};
     preargv[i] = argv[i];
     i++;
   }
-  int argc=i;//重定向前面参数的个数
+  int preargc=i;//重定向前面参数的个数
   i++;
   //出现 echo "adcbe" > test.c  这种情况
   int fdout = dup(1);//让标准输出获取一个新的文件描述符
@@ -173,16 +176,15 @@ char *preargv[MAX] = {NULL};
   {
     if (_pipe=1) //管道'|'
     {
-      mymulpipe(preargv, argc);
+      mymulpipe(preargv, preargc);
     }
     else
      execvp(preargv[0], preargv);
   }
   else if (pid > 0)//父进程
   {
-     if(backpro==1)
+     if(pass==1)
       {
-        backpro=0;
         printf("%d\n",pid);
         return;
       }
@@ -190,6 +192,7 @@ char *preargv[MAX] = {NULL};
   }
   dup2(fdout, 1);//
 }
+
 void aoredir(char *argv[]){
 
   char *preargv[MAX] = {NULL};
@@ -200,7 +203,7 @@ void aoredir(char *argv[]){
     preargv[i] = argv[i];
     i++;
   }
-  int argc=i;//重定向前面参数的个数
+  int preargc=i;//重定向前面参数的个数
   i++;
   int fdout = dup(1);//让标准输出获取一个新的文件描述符
   int fd = open(argv[i], O_WRONLY | O_CREAT | O_APPEND,0666); //只写模式|表示如果指定文件不存在，则创建这个文件|表示追加，如果原来文件里面有内容，则这次写入会写在文件的最末尾。
@@ -208,30 +211,172 @@ void aoredir(char *argv[]){
    dup2(fd, 1);
   if (pid == 0) //子进程
   {
-    if (flag == 3) //管道'|'
+    if (_pipe == 1) //管道'|'
       {
-        mymulpipe(strc, number);
+        mymulpipe(preargv, preargc);
       }
       else
        execvp(preargv[0], preargv);
   }
   else if (pid > 0)
   {
-     if(backp==1)
+     if(pass==1)
       {
-        backp=0;
         printf("%d\n",pid);
         return;
       }
     waitpid(pid, NULL, 0);
   }
-  dup2(fdout, 1); 
+  dup2(fdout, 1);
 }
+
 void iredir(char *argv[]){
-
+char *preargv[MAX] = {NULL};
+  int i = 0;
+  while (strcmp(argv[i], "<"))
+  {
+    preargv[i] = argv[i];
+    i++;
+  }
+  i++;
+  int preargc=i;//重定向前面参数的个数
+  int fdin = dup(0);//让标准输入获取一个新的文件描述符
+  int fd = open(argv[i], O_RDONLY,0666); //只读模式
+   dup2(fd, 0);
+  pid_t pid = fork();
+  if (pid == 0) //子进程
+  {
+     if (_pipe == 1) //管道'|'
+      {
+        mymulpipe(preargv, preargc);
+      }
+      else if(o_redir==1)
+        oredir(preargv);
+      else if(a_o_redir==1)
+        aoredir(preargv);
+      else
+        xecvp(preargv[0], preargv);
+  }
+  else if (pid > 0)
+  {
+    waitpid(pid, NULL, 0);
+  }
+  dup2(fdin, 0);
 }
-void mymulpipe(char *argv[], int argc ){
 
+void mymulpipe(char *argv[], int argc ){
+pid_t pid;
+  int index[10];//存放每个管道的下标
+  int number=0;//统计管道个数
+  for(int i=0;i<argc;i++)
+    if(!strcmp(argv[i],"|")) index[number++]=i;
+  int cmd_count=number+1;//命令个数
+  char* cmd[cmd_count][10];
+  for(int i=0;i<cmd_count;i++)//将命令以管道分割存放组数组里
+  {
+    if(i==0)//第一个命令
+    {
+      int n=0;
+      for(int j=0;j<index[i];j++)
+      {
+        cmd[i][n++]=argv[j];
+      }
+      cmd[i][n]=NULL;
+    }
+    else if(i==number)//最后一个命令
+    {
+      int n=0;
+      for(int j=index[i-1]+1;j<argc;j++)
+      {
+        cmd[i][n++]=argv[j];
+      }
+      cmd[i][n]=NULL;
+    }
+    else 
+    {
+      int n=0;
+      for(int j=index[i-1]+1;j<index[i];j++)
+      {
+        cmd[i][n++]=argv[j];
+      }
+      cmd[i][n]=NULL;
+    }
+  }//经过上述操作，我们已经将指令以管道为分隔符分好,下面我们就可以创建管道了
+  int fd[number][2];  //存放管道的描述符
+  for(int i=0;i<number;i++)//循环创建多个管道
+  {
+    pipe(fd[i]);
+  }
+  int i=0;
+  for(i=0;i<cmd_count;i++)//父进程循环创建多个并列子进程
+  {
+    pid=fork();
+    if(pid==0)//子进程直接退出循环，不参与进程的创建
+    break;
+  }
+  if(pid==0)//子进程
+  {
+    if(number)
+    {
+      if(i==0)//第一个子进程
+      {
+        dup2(fd[0][1],1);//绑定写端`  
+        close(fd[0][0]);//关闭读端
+        //其他进程读写端全部关闭
+        for(int j=1;j<number;j++)
+        {
+          close(fd[j][1]);
+          close(fd[j][0]);
+        }
+      }
+      else if(i==number)//最后一个进程
+      {
+        dup2(fd[i-1][0],0);//打开读端
+        close(fd[i-1][1]);//关闭写端
+         //其他进程读写端全部关闭
+        for(int j=0;j<number-1;j++)
+        {
+          close(fd[j][1]);
+          close(fd[j][0]);
+        }
+      }
+      else //中间进程
+      {
+        dup2(fd[i-1][0],0);//前一个管道的读端打开
+        close(fd[i-1][1]);//前一个写端关闭
+        dup2(fd[i][1],1);//后一个管道的写端打开
+        close(fd[i][0]);//后一个读端关闭
+        //其他的全部关闭
+        for(int j=0;j<number;j++)
+        {
+           if(j!=i&&j!=(i-1))
+           {
+             close(fd[j][0]);
+             close(fd[j][1]);
+           }
+        }
+      }
+    }
+ 
+    execvp(cmd[i][0],cmd[i]);//执行命令
+    perror("execvp");
+    exit(1);
+  }
+  //父进程什么都不干，把管道的所有口都关掉
+    for(i=0;i<number;i++)
+    {
+        close(fd[i][0]);
+        close(fd[i][1]);//父进程端口全部关掉
+
+    }
+     if(pass==1)
+      {
+        pass=0;
+        printf("%d\n",pid);
+        return;
+      }
+  for(int j=0;j<cmd_count;j++)//父进程等待子进程
+  wait(NULL);
 }
 void clear_para(){
 cd =0;
